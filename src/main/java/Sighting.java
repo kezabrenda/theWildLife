ce346c17350b1e34ccede680331d1d2a20d397ad
keza1brenda <keza1brenda@gmail.com>
2021-01-04 16:11:32 +0200

    add sighting class

---
+++ src/main/java/Sighting.java
@@ -0,0 +1,23 @@
+public class Sighting {
+    private String animal;
+    private String location;
+    private String rangerName;
+
+    public Sighting(String animal, String location, String rangerName){
+        this.animal=animal;
+        this.location=location;
+        this.rangerName=rangerName;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
+}
